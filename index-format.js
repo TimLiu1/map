cf59c19355a2ac95fed5b371cac521837608c65d
zukliu <zukliu@ebay.com>
2019-02-26 17:50:32 +0800

    format

---
+++ index-format.js
@@ -0,0 +1,305 @@
+function start(url) {
+    RequestGet('/network/list', (err, data) => {
+        var cities = data.data;
+        var citiesData = getCityData(cities);
+        rendMap(cities, citiesData)
+        // return citiesData;
+    })
+}
+
+function getCityData(cities) {
+    let result = []
+    for (var i = 0; i < cities.length; i++) {
+        if (cities[i].city != null) {
+            result.push(
+                {
+                    "id": cities[i].id,
+                    "type": "Feature",
+                    "properties": { "name": cities[i].city },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [cities[i].longitude, cities[i].latitude]
+                    }
+                })
+        }
+    }
+    return result;
+}
+
+function getLineData(cities) {
+    let result = [];
+    cities.forEach(function (e, i, value) {
+        if (e.city !== null) {
+            result.push({
+                "id": e.id,
+                "type": "Feature",
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [[e.longitude, e.latitude], [e.MainLongitude, e.MainLatitude]]
+                }
+            })
+        }
+    })
+    return result;
+}
+
+function linetransition(path) {
+    path.transition()
+        .duration(1000)
+        .attrTween("stroke-dasharray", function () {
+            var len = this.getTotalLength();
+            return function (t) {
+                return (d3.interpolate('0,' + len, len + ',0'))(t)
+            };
+        })
+}
+
+function connectLine(links, linksData, path, circle,cities) {
+    let linksFeatures = links.selectAll("path")
+        .data(linksData)
+    linksFeatures.enter()
+        .append("path")
+        .attr("class", "arcs")
+        .on('mouseover', () => {
+            console.log('hello')
+        })
+        .attr("d", function (d) {
+            return path(circle.clip(d));
+        })
+        .on('mouseover', function () {//选择所有的点添加点击事件
+            var id = $(this).attr('id');
+            cities.forEach((city) => {
+                if (id == city.id) {
+                    renderCard(city)
+                }
+            })
+        }).attr("id", function (d) {
+            return d.id
+        })
+        .call(linetransition)
+    return linksFeatures;
+}
+
+function rendMap(cities, citiesData) {
+    (function (w, d3, undefined) {
+        "use strict";
+        var width, height;
+        function getSize() {
+            width = w.innerWidth,
+                height = w.innerHeight;
+            if (width === 0 || height === 0) {
+                setTimeout(function () {
+                    getSize();
+                }, 100);
+            }
+            else {
+                init();
+            }
+        }
+
+        function init() {
+            //Setup path for outerspace
+            var space = d3.geo.azimuthal()
+                .mode("equidistant")
+                .translate([width / 2, height / 2]);
+            space.scale(space.scale() * 3);
+
+            var spacePath = d3.geo.path()
+                .projection(space)
+                .pointRadius(1);
+
+            //Setup path for globe
+            var projection = d3.geo.azimuthal()
+                .mode("orthographic")
+                .translate([width / 2, height / 2]);
+
+            var scale0 = projection.scale();
+            var path = d3.geo.path()
+                .projection(projection)
+                .pointRadius(2);
+
+            //Setup zoom behavior
+            var zoom = d3.behavior.zoom(true)
+                .translate(projection.origin())
+                .scale(projection.scale())
+                .scaleExtent([300, 800])
+                .on("zoom", move);
+
+            var circle = d3.geo.greatCircle();
+            var svg = d3.select("body")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .call(zoom)
+                .on("dblclick.zoom", null);
+
+            //Create a list of random stars and add them to outerspace
+            var starList = createStars(2000);
+            var stars = svg.append("g")
+                .selectAll("g")
+                .data(starList)
+                .enter()
+                .append("path")
+                .attr("class", "star")
+                .attr("d", function (d) {
+                    spacePath.pointRadius(d.properties.radius);
+                    return spacePath(d);
+                });
+
+            svg.append("rect")
+                .attr("class", "frame")
+                .attr("width", width)
+                .attr("height", height);
+
+            //Create the base globe
+            var backgroundCircle = svg.append("circle")
+                .attr('cx', width / 2)
+                .attr('cy', height / 2)
+                .attr('r', projection.scale())
+                .attr('class', 'globe')
+                .attr("filter", "url(#glow)")
+                .attr("fill", "url(#gradBlue)");
+
+            var g = svg.append("g"),
+                features;
+            var point = svg.append("g"),
+                devFeatures;
+
+            var label = svg.append("g"),
+                lableFeatures;
+
+            var links = svg.append("g"),
+                linksFeatures;
+
+            //Add all of the countries to the globe
+            d3.json("world-countries.json", function (collection) {
+                features = g.selectAll(".feature").data(collection.features);
+
+                features.enter().append("path")
+                    .attr("class", "feature")
+                    .attr("d", function (d) { return path(circle.clip(d)); });
+            });
+            // cities marker
+            devFeatures = point.selectAll(".dev")
+                .data(citiesData);
+            devFeatures.enter()
+                .append("path")
+                .attr("class", "dev")
+                .on('click', function () {//选择所有的点添加点击事件
+                    var id = $(this).attr('id');
+                    cities.forEach((city) => {
+                        if (id == city.id) {
+                            renderCardLocation(city)
+                        }
+                    })
+                    start('/network/list?mainDevice=pvg0-wr03')
+                })
+                .attr("d", function (d) {
+                    return path(circle.clip(d));
+                }).attr("id", function (d) {
+                    return d.id
+                });;
+            // cities connection
+            var linksData = getLineData(cities)
+            linksFeatures = connectLine(links, linksData, path, circle,cities)
+
+            //cities label
+            lableFeatures = label.selectAll("text")
+                .data(citiesData)
+
+            lableFeatures.enter()
+                .append("text")
+                .attr("class", "label")
+                .text(function (d) {
+                    return d.properties.name
+                })
+            labels()
+            function labels() {
+                var centerPos = projection.invert([width / 2, height / 2]);
+                var arc = d3.geo.greatArc();
+                lableFeatures
+                    .attr("transform", function (d) {
+                        var loc = projection(d.geometry.coordinates),
+                            x = loc[0],
+                            y = loc[1];
+                        var offset = 5;
+                        return "translate(" + (x + offset) + "," + (y - 3) + ")"
+                    })
+                    .style("display", function (d) {
+                        var d = arc.distance({ source: d.geometry.coordinates, target: centerPos });
+                        return (d > 1.57) ? 'none' : 'inline';
+                        return 'inline';
+                    });
+            }
+            //Redraw all items with new projections
+            function redraw() {
+                features.attr("d", function (d) {
+                    return path(circle.clip(d));
+                });
+                devFeatures.attr("d", function (d) {
+                    return path(circle.clip(d));
+                });
+                lableFeatures.attr("d", function (d) {
+                    return path(circle.clip(d));
+                })
+                linksFeatures.attr("d", function (d) {
+                    return path(circle.clip(d));
+                })
+                stars.attr("d", function (d) {
+                    spacePath.pointRadius(d.properties.radius);
+                    return spacePath(d);
+                });
+                labels()
+            }
+
+
+            function move() {
+                if (d3.event) {
+                    var scale = d3.event.scale;
+                    var origin = [d3.event.translate[0] * -1, d3.event.translate[1]];
+
+                    projection.scale(scale);
+                    space.scale(scale * 3);
+                    backgroundCircle.attr('r', scale);
+                    path.pointRadius(2 * scale / scale0);
+
+                    projection.origin(origin);
+                    circle.origin(origin);
+
+                    //globe and stars spin in the opposite direction because of the projection mode
+                    var spaceOrigin = [origin[0] * -1, origin[1] * -1];
+                    space.origin(spaceOrigin);
+                    redraw();
+                }
+            }
+
+
+            function createStars(number) {
+                var data = [];
+                for (var i = 0; i < number; i++) {
+                    data.push({
+                        geometry: {
+                            type: 'Point',
+                            coordinates: randomLonLat()
+                        },
+                        type: 'Feature',
+                        properties: {
+                            radius: Math.random() * 1.5
+                        }
+                    });
+                }
+                return data;
+            }
+
+            function randomLonLat() {
+                return [Math.random() * 360 - 180, Math.random() * 180 - 90];
+            }
+        }
+
+        getSize();
+
+    }(window, d3));
+}
+
+start('http://localhost:8008/network/list');
